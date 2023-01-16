cc03b9c5171705e748fa5f8ac9d569a81b770845
anglyan <angel.yanguas@gmail.com>
2023-01-15 21:48:28 -0600

    base_url to github pages

--- docs/conf.py
+++ docs/conf.py
@@ -55,3 +55,4 @@ html_theme = 'nature'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_baseurl = "https://spikelearn.github.io/spikelearn/"
