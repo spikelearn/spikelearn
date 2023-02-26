8d03f4441e9e293f68530795d160a3b2cb630e2b
anglyan <angel.yanguas@gmail.com>
2023-02-26 14:24:38 -0600

    updated version v0.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath("../src"))
 project = 'Spikelearn'
 copyright = '2022, Argonne'
 author = 'Argonne'
-release = '0.1.1'
+release = '0.1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
